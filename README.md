19f6a5bab80b9c331090bb3556a4411ae36900c0
huytry <227800957+Shadow-Martial@users.noreply.github.com>
2025-09-09 10:38:12 +0545

    Reverse

--- README.md
+++
@@ -1,2 +0,0 @@
-# pixer-api
-
--- config/filesystems.php
+++ config/filesystems.php
@@ -53,20 +53,6 @@ return [
             'visibility' => 'public',
         ],
 
-        'ftp' => [
-            'driver' => 'ftp',
-            'host' => env('FTP_HOST'),
-            'username' => env('FTP_USERNAME'),
-            'password' => env('FTP_PASSWORD'),
-            'port' => env('FTP_PORT', 21),
-            'root' => env('FTP_ROOT', '/'),
-            'passive' => env('FTP_PASSIVE', true),
-            'ssl' => env('FTP_SSL', false),
-            'timeout' => 30,
-            'url' => env('FTP_URL'),
-            'visibility' => 'public',
-        ],
-
     ],
 
     /*
